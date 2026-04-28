ec2d22f36f2c6fb3b71f299a279461300bfeee06
weisscorp <49550074+weisscorp@users.noreply.github.com>
2026-04-28 12:54:49 +0400

    chore: regenerate OpenAPI and Python API for v2.4.2

--- api/v1beta2/zz_generated.openapi.go
+++ api/v1beta2/zz_generated.openapi.go
@@ -1816,6 +1816,20 @@ func schema_spark_operator_v2_api_v1beta2_RestartPolicy(ref common.ReferenceCall
 							Format:      "",
 						},
 					},
+					"retryIntervalMethod": {
+						SchemaProps: spec.SchemaProps{
+							Description: "RetryIntervalMethod specifies how retry interval should be calculated. Supported values are: - linear: retry interval is multiplied by number of attempts (linear backoff). - static: retry interval is always constant.",
+							Type:        []string{"string"},
+							Format:      "",
+						},
+					},
+					"retryInterval": {
+						SchemaProps: spec.SchemaProps{
+							Description: "RetryInterval is the interval in seconds between retries. If set, it takes precedence over OnFailureRetryInterval and OnSubmissionFailureRetryInterval.",
+							Type:        []string{"integer"},
+							Format:      "int64",
+						},
+					},
 					"onSubmissionFailureRetries": {
 						SchemaProps: spec.SchemaProps{
 							Description: "OnSubmissionFailureRetries is the number of times to retry submitting an application before giving up. This is best effort and actual retry attempts can be >= the value specified due to caching. These are required if RestartPolicy is OnFailure.",
