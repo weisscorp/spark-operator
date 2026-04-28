6d047c1f0fc59ff80001197a04400696322667af
weisscorp <49550074+weisscorp@users.noreply.github.com>
2026-04-28 13:02:54 +0400

    chore: align codegen version with client-go

--- Makefile
+++ Makefile
@@ -61,7 +61,7 @@ GEN_CRD_API_REFERENCE_DOCS_VERSION ?= v0.3.0
 HELM_VERSION ?= $(shell grep -e '^	helm.sh/helm/v3 v' go.mod | cut -d ' ' -f 2)
 HELM_UNITTEST_VERSION ?= 0.8.2
 HELM_DOCS_VERSION ?= v1.14.2
-CODE_GENERATOR_VERSION ?= v0.33.1
+CODE_GENERATOR_VERSION ?= v0.32.5
 
 ## Binaries
 SPARK_OPERATOR ?= $(LOCALBIN)/spark-operator
--- pkg/client/clientset/versioned/typed/api/v1beta2/api_client.go
+++ pkg/client/clientset/versioned/typed/api/v1beta2/api_client.go
@@ -49,7 +49,9 @@ func (c *SparkoperatorV1beta2Client) SparkApplications(namespace string) SparkAp
 // where httpClient was generated with rest.HTTPClientFor(c).
 func NewForConfig(c *rest.Config) (*SparkoperatorV1beta2Client, error) {
 	config := *c
-	setConfigDefaults(&config)
+	if err := setConfigDefaults(&config); err != nil {
+		return nil, err
+	}
 	httpClient, err := rest.HTTPClientFor(&config)
 	if err != nil {
 		return nil, err
@@ -61,7 +63,9 @@ func NewForConfig(c *rest.Config) (*SparkoperatorV1beta2Client, error) {
 // Note the http client provided takes precedence over the configured transport values.
 func NewForConfigAndClient(c *rest.Config, h *http.Client) (*SparkoperatorV1beta2Client, error) {
 	config := *c
-	setConfigDefaults(&config)
+	if err := setConfigDefaults(&config); err != nil {
+		return nil, err
+	}
 	client, err := rest.RESTClientForConfigAndClient(&config, h)
 	if err != nil {
 		return nil, err
@@ -84,7 +88,7 @@ func New(c rest.Interface) *SparkoperatorV1beta2Client {
 	return &SparkoperatorV1beta2Client{c}
 }
 
-func setConfigDefaults(config *rest.Config) {
+func setConfigDefaults(config *rest.Config) error {
 	gv := apiv1beta2.SchemeGroupVersion
 	config.GroupVersion = &gv
 	config.APIPath = "/apis"
@@ -93,6 +97,8 @@ func setConfigDefaults(config *rest.Config) {
 	if config.UserAgent == "" {
 		config.UserAgent = rest.DefaultKubernetesUserAgent()
 	}
+
+	return nil
 }
 
 // RESTClient returns a RESTClient that is used to communicate
